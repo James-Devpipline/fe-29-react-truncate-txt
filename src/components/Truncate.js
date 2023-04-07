7e36bdc8be826a1dcbfad884691b08f0657d5e0e
james <james@devpipeline.com>
2023-04-07 14:09:56 -0600

    fixed slicing

--- src/components/Truncate.js
+++ src/components/Truncate.js
@@ -1,9 +1,9 @@
 export default function Truncate(props) {
   if (props.myStr.length > props.num) {
     if(props.etc){
-      return props.myStr.slice(0, props.num) + props.etc
+      return props.myStr.slice(0, props.num + 1) + props.etc
     } else {
-      return props.myStr.slice(0, props.num) + '...'
+      return props.myStr.slice(0, props.num + 1) + '...'
     }
   } else {
     return props.myStr
